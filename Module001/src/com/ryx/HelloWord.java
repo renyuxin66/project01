b03906a59a66c34830eb88e74e58b2ecd3064d2d
tom_glb <goodMorning_glb@atguigu.com>
2019-03-17 17:48:31 +0800

    Initial commit

---
+++ Module001/src/com/ryx/HelloWord.java
@@ -0,0 +1,27 @@
+package com.ryx;
+
+import java.util.Date;
+
+/**
+ * @
+ * xkkjd
+ */
+public class HelloWord {
+
+    //dhfhajhfdz
+
+    /**
+     * jhdfakhf
+     * @param args
+     */
+
+    public  static  void  main(String [] args){
+        System.out.println("helloWorld!!!");
+
+        System.out.println("helloWorld!!!");
+        System.out.println("helloWorld!");
+        Date date = new Date();
+
+
+    }
+}
